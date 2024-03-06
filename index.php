0f182985431c7b6c59817ad084416b95bb3b7d0f
angelarcia <uriarcia@gmail.com>
2024-03-05 18:52:17 -0600

    feat: 17 PDO first steps

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 
-
 require 'functions.php';
-
-$uri = $_SERVER['REQUEST_URI'];
-if ($uri === '/') {
-    require 'controllers/index.php';
-} else if ($uri === '/about') {
-    require 'controllers/about.php';
-}
+require 'router.php';
